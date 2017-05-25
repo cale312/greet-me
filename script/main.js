b4e2841873b2235409c6e96438986e4a068df1c2
cale312 <zikisacalesogaxa@gmail.com>
2017-05-25 11:44:06 +0200

    main js file for compiling all the functions and displaying to the dom

--- script/main.js
+++ script/main.js
@@ -1,39 +1,7 @@
 'use strict';
-var greets = 0;
-document.getElementById('greets').innerHTML = greets;
 
 var greet = document.querySelector('#click');
-var namesGreeted = {};
 
-for (let i = 0; i < namesGreeted.length; i++) {}
-
-function clearGreeting() {
-  var greeting;
-  greeting = document.getElementById('demo').innerHTML;
-  if (greeting.length > 0) {
-    document.getElementById('demo').innerHTML = '____________';
-  }
-}
-
-function resetFunction() {
-  'use strict';
-  var x = document.getElementById("greets");
-  if (x >= 0) {
-    document.getElementById("greets").innerHTML = greets = 0;
-  }
-  localStorage.clear();
-}
-
-// Call the functions that return the name greeted and the massage
-function displayGreeting(){
-  var lang = document.getElementsByName('lang');
-  var name = document.querySelector('#text');
-  var result = greetMassage(lang);
-  var greetName = getName(name);
-
-  document.querySelector('#demo').innerHTML = result + ' ' + greetName;
-}
-
-greet.addEventListener('click', function(){
-  displayGreeting();
+greet.addEventListener('click', function() {
+  document.querySelector('#demo').innerHTML = displayGreeting();
 });
