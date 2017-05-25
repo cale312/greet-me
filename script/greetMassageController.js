e6b124ba60d9a9a11aa229ad13d5393f42f27973
cale312 <zikisacalesogaxa@gmail.com>
2017-05-25 11:44:52 +0200

    controller for getting the right greeting massage depending on which radio button checked

--- script/greetMassageController.js
+++ script/greetMassageController.js
@@ -1,11 +1,13 @@
-// Checks which radio has been checked and returns the right greeting massage in that language that has been selected..
-function greetMassage(lang){
-  for (let i = 0; i < lang.length; i++){
-    if(lang[i].checked && lang[i].value === 'english'){
+'use strict';
+
+// Checks which radio has been checked and returns the right greeting massage in that language that has been selected...
+function greetMassage(lang) {
+  for (let i = 0; i < lang.length; i++) {
+    if (lang[i].checked && lang[i].value === 'english') {
       return 'Hello'
-    } else if(lang[i].checked && lang[i].value === 'xhosa'){
+    } else if (lang[i].checked && lang[i].value === 'xhosa') {
       return 'Molo'
-    } else if(lang[i].checked && lang[i].value === 'espanol'){
+    } else if (lang[i].checked && lang[i].value === 'espanol') {
       return 'Hola'
     }
   }
