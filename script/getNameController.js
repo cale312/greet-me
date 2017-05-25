058103d9d77be2cd19618613edb13ea1f413fd2f
cale312 <zikisacalesogaxa@gmail.com>
2017-05-25 11:42:19 +0200

    controller for getting the name to greet

--- script/getNameController.js
+++ script/getNameController.js
@@ -1,10 +1,18 @@
-// Takes in the name inputed...
-function getName(name){
-  if (name.value !== '' && namesGreeted[name] === !undefined){
+'use strict';
+
+// Returns the name in the input box...
+function getName(name) {
+  var namesGreeted = {};
+  var greetedNames = [];
+
+  for (let i = 0; i < namesGreeted.length; i++) {}
+  if (name.value !== '' && namesGreeted[name] === !undefined) {
     return name.value;
-  } else if(name.value !== '' && namesGreeted[name] === undefined){
+  } else if (name.value !== '' && namesGreeted[name] === undefined) {
+    namesGreeted[name] = 1;
+    greetedNames.push(name.value);
     return name.value;
-  } else{
+  } else {
     return 'Please enter a valid name';
   }
 }
