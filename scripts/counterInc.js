c71425872b5a6299bfcc8162254a74792d007ba5
cale312 <zikisacalesogaxa@gmail.com>
2017-05-31 15:00:17 +0200

    removed the else statement for when the name is undefined

--- scripts/counterInc.js
+++ scripts/counterInc.js
@@ -2,15 +2,12 @@
 
 var counter = 0;
 var greetedNames = {};
-for (let i = 0; i < greetedNames.length; i++) {}
 
 // counter
-function counterInc(newName) {
-  if (greetedNames[newName] === undefined && newName.value !== '') {
-    counter++;
+function counterInc(newName){
+  if (greetedNames[newName] === undefined && newName !== '') {
+    counter += 1;
     greetedNames[newName] = 1;
-  } else if (greetedNames[newName] !== undefined && newName.value !== '') {
-    counter = counter;
   }
   return counter;
 }
