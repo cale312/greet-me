3509b537bd62520a7c2a20dfa3f2890054d6206a
cale312 <zikisacalesogaxa@gmail.com>
2017-05-31 16:23:05 +0200

    added a before hook for the test

--- test/counterInc.test.js
+++ test/counterInc.test.js
@@ -1,7 +1,11 @@
 describe('Counter incremetion...', function(){
 
+  before(function(){
+    counter --;
+  });
+
   it('should increment the counter by one if name is being greeted for the first time', function(){
-    assert.equal(1, counterInc('Cale'));
+    assert.equal(counter + 1, counterInc('Cale'));
   });
 
 });
