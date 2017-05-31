a54dca1ee0808869ce6c3bf3f40764f93f0e959d
cale312 <zikisacalesogaxa@gmail.com>
2017-05-31 10:29:02 +0200

    counter test

---
+++ test/counterInc.test.js
@@ -0,0 +1,7 @@
+describe('Counter incremetion...', function(){
+
+  it('should increment the counter by one if name is being greeted for the first time', function(){
+    assert.equal(1, counterInc('Cale'));
+  });
+
+});
