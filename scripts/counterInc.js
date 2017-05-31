f9c33c074f38fa2ae5d1e97007941cdecae6a094
cale312 <zikisacalesogaxa@gmail.com>
2017-05-31 13:08:33 +0200

    added loop through the namesGreeted object

--- scripts/counterInc.js
+++ scripts/counterInc.js
@@ -1,13 +1,16 @@
+'use strict';
+
 var counter = 0;
 var greetedNames = {};
+for (let i = 0; i < greetedNames.length; i++) {}
 
 // counter
-function greetCounter(newName){
-    if (greetedNames[newName] === undefined && newName.value !==''){
-        counter =  counter += 1;
-        greetedNames[newName] = 1;
-    } else if(greetedNames[newName] === undefined && newName.value !=='') {
-        counter = counter;;
-    }
-    return counter;
+function counterInc(newName) {
+  if (greetedNames[newName] === undefined && newName.value !== '') {
+    counter++;
+    greetedNames[newName] = 1;
+  } else if (greetedNames[newName] !== undefined && newName.value !== '') {
+    counter = counter;
+  }
+  return counter;
 }
