4c29ca4c9007fac628cce3222b2293840fca3c74
cale312 <zikisacalesogaxa@gmail.com>
2017-05-31 15:02:51 +0200

    added the name as string argument on the counterInc function

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,11 @@ var lang = document.querySelectorAll('.lang');
 
 // compile the greeting sentence
 function displayGreeting(){
-    var greetName = getName(newName).value;
+    var greetName = getName(newName);
     var checkedBtn = getRadioButton(lang);
     var greetMassage = getMassage(checkedBtn);
     if (greetName !== undefined && greetMassage !== undefined){
-        return greetMassage + ' ' + greetName;
+        return greetMassage + ' ' + greetName.value;
     } else {
         return 'Please enter a valid name and choose a language you want to be greeted in';
     }
@@ -19,7 +19,7 @@ function displayGreeting(){
 // send results to the DOM
 greet.addEventListener('click', function(){
     document.querySelector('.display').innerHTML = displayGreeting();
-    document.querySelector('.counter').innerHTML = counterInc(newName);
+    document.querySelector('.counter').innerHTML = counterInc(newName.value);
     newName.value = '';
 });
 
