42f9d54186c480c86df6b97f94719e1df4e8bd17
cale312 <zikisacalesogaxa@gmail.com>
2017-05-31 15:01:32 +0200

    why the hell did you loop throught the returned checked radio button

--- scripts/getMassage.js
+++ scripts/getMassage.js
@@ -1,14 +1,12 @@
 'use strict';
 
 // Get checked radio button language
-function getMassage(checkedBtn){
-    for (let i = 0; i < checkedBtn.length; i++){
-        if (checkedBtn === 'eng'){
-            return 'Hello ';
-        } else if (checkedBtn === 'xho'){
-            return 'Molo ';
-        } else if (checkedBtn === 'esp'){
-            return 'Hola ';
-        }
-    }
+function getMassage(checkedBtn) {
+  if (checkedBtn === 'eng') {
+    return 'Hello ';
+  } else if (checkedBtn === 'xho') {
+    return 'Molo ';
+  } else if (checkedBtn === 'esp') {
+    return 'Hola ';
+  }
 }
